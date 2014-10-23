e8a0fb64b50ecc4e4598ae9f581f5ec84aad1056
mjozwiak <mjozwiak@sigma.ug.edu.pl>
2014-10-23 13:11:00 +0200

    rubyfiles.rb
    script in .rb file

---
+++ filesinY.rb
@@ -0,0 +1,45 @@
+#learn ruby io file system in Y minutes
+
+
+#creating a new file variable
+new_file = File.new("file.txt")#creates a new instance of the File class
+
+#cheching if a file exists
+File.exists?('existing_file.txt') #returns true if existing_file.txt exists in script's directory
+puts new_file.size #prints the size of a file
+
+#file access modes
+File.open('file.txt','w') #creates an empty file with a name file.txt. If the file has already existed, it's deleted and replaced with a new one
+File.open('file.txt','r') #can open only an already existing file
+
+#writing to a text file
+writing_file = File.open('writing.txt','w')
+writing_file.write "writing text\nAnd in a new line" #writes 2 lines into writing.txt
+writing_file.write 'writing text\nAnd in a new line' #it will uppend the second line(\n will not be treated as a new line character)
+writing_file.close #closing an opened file
+
+#deleting a file 
+File.delete("delfile.txt","delfile1.txt") #deletes all files listed, if has permission
+
+#reading from a text file
+read_file = File.open("file.txt","r")
+
+#reading file line by line
+while line = reading_file.gets
+	puts line #prints every line from a file in a new line
+end
+
+#reading file by characters
+read_file.each_char { |character|
+	puts character #prints every character from a file in a new line
+}
+
+#reading file by bytes
+read_file.each_byte { |encoding|
+	puts encoding #prints encoding of every character from a file in a new line
+}
+read_file.close
+
+
+
+
